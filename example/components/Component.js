3f2e12f4ab80706107c652148fbd7ddd110ce8f3
Kyei, Alexander <akyei@cvent.com>
2019-06-26 09:42:07 -0400

    Changed example files

--- example/components/Component.js
+++ example/components/Component.js
@@ -2,7 +2,11 @@ const React = require('react');
 const Foo = require('Foo');
 
 /**
- * General component description.
+ * General component description. Tag 1 is a string description. Tag 2 is a boolean since it
+ * has no text on the same line.
+ * @Tag1 This is a basic tag description
+ * @Tag2
+ *
  */
 const Component = React.createClass({
   displayName: 'Component',
@@ -10,7 +14,11 @@ const Component = React.createClass({
   propTypes: {
     ...Foo.propTypes,
     /**
-     * Prop description
+     * Appearance: Prop description. This prop will have create control set to true
+     * since it has the string "Appearance:"
+     * @Option1 This tag will appear under fields for this prop
+     * @Option2
+     *
      */
     bar: React.PropTypes.number,
   },
